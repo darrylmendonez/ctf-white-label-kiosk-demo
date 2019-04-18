88fade094e674d209c07cd1e5347ff246e42912f
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-18 13:20:28 -0400

    updated container class to go back to normal bootstrap width

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,15 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-    `
-      :host {
-        margin: 0 auto;
-        max-width: 1814px;
-        width: 100%;
-      }
-    `,
-  ],
 })
 export class NavbarComponent implements OnInit {
   constructor() {}
